cd0cb0db81b75cb349fab28768bb967aaf6d0ad0
Emma Florenzano <emma.florenzano@gmail.com>
2021-09-03 16:38:09 +0200

    Player shoots now

--- Units/Player/Player.gd
+++ Units/Player/Player.gd
@@ -3,11 +3,12 @@ extends KinematicBody2D
 export var speed := 500.0
 var velocity := Vector2.ZERO
 var life := 50
-
+export var projectile: PackedScene
+var shootTimer: Timer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	shootTimer = get_node("ShootTimer")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -24,5 +25,20 @@ func _physics_process(delta: float):
 		velocity += Vector2.UP
 	if Input.is_action_pressed("ui_down"):
 		velocity += Vector2.DOWN
+	if Input.is_action_just_pressed("shoot"):
+		shootTimer.start()
+	if Input.is_action_just_released("shoot"):
+		shootTimer.stop()
 	velocity = velocity.normalized() * speed
 	var collision = move_and_collide(velocity * delta)
+
+func shoot():
+	if projectile == null:
+		return
+	var new_p = projectile.instance()
+	get_parent().add_child(new_p)
+	new_p.global_position = position + Vector2(0, -15)
+
+
+func _on_ShootTimer_timeout():
+	shoot()
